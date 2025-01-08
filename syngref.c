5ccd782a26e67fc82fa94b2263d7278b1d733a3c
Anant Maheshwari <anant.maheshwari.76@gmail.com>
2025-01-08 17:38:53 +0000

    basic structure; TODO generating information about syncmer occurences

--- syngref.c
+++ syngref.c
@@ -2,10 +2,10 @@
  *  Author: Anant Maheshwari (am3320@cam.ac.uk)
  *  Copyright (C) Richard Durbin, Cambridge University, 2024
  *-------------------------------------------------------------------
- * Description: determine locations of syncmers in a <syng>.1khash within a reference (.fa) genome
+ * Description: determine locations of syncmers from a <syng>.1khash within a reference (.fa) genome
  * Exported functions:
  * HISTORY:
- * Last edited: Jan  7 11:16 2025 (am3320)
+ * Last edited: Jan  8 15:29 2025 (am3320)
  * Created: Mon Jan  6 14:51  2024 (am3320)
  *-------------------------------------------------------------------
  */
@@ -43,31 +43,28 @@ static void checkParams (OneFile *of, Params *p)
 	 oneInt(of,0), oneInt(of,1), oneInt(of,2), p->k, p->w, p->seed) ;
 }
 
-
 /**************** main program ********************/
 
 static char *usage = 
   "Usage: syngref [options]* <.1khash file> <reference .fa file>\n"
   "possible options are:\n"
-  "  -T <threads>           : [8] number of threads\n"
   "  -o <outfile prefix>    : default is syngref, file type is .1ref\n";
 
 int main (int argc, char *argv[])
 {
-    fprintf(stdout, "Hello, world!\n");
-
     storeCommandLine (argc--, argv++) ;
     timeUpdate (0) ;
 
     if (!argc) { fprintf (stderr, "%s", usage) ; exit (0) ; }
-      char *outPrefix = "syngref" ;
-
-    if (argc != 2) die ("missing the three required arguments\n%s", usage) ;
+    char *outPrefix = "syngref" ;
+    int nThread = 1 ;
+    printf("arguments: %d %s %s \n", argc, argv[0], argv[1]);
+    if (argc != 2) die ("missing the two required arguments\n%s", usage) ;
 
     OneSchema *schema = oneSchemaCreateFromText (syngSchemaText) ;
     if (!schema) die ("problem creating schema - needs debugging/recompilation") ;
     
-      // open all the files here, so we can die quickly if any file opens fail
+    // open all the files here
     OneFile *ofK = oneFileOpenRead (argv[0], schema, "khash", 1) ;
     if (!ofK) die ("failed to open .1khash file %s", argv[0]) ;
 
@@ -75,7 +72,11 @@ int main (int argc, char *argv[])
     if (!sio) die ("failed to open sequence file %s", argv[1]) ;
     
     // set up output file here 
-    // TODO - add output file
+    OneFile *ofOut = oneFileOpenWriteNew (fnameTag(outPrefix,"1ref"), schema, "ref", true, nThread) ;
+    if (!ofOut) die ("failed to open output file %s", argv[2]) ;
+    oneAddProvenance (ofOut, "syngref", SYNG_VERSION, getCommandLine()) ;
+    oneAddReference (ofOut, argv[0], 1) ;
+    oneAddReference (ofOut, argv[1], 2) ;
 
     // read the khash  
     Params     params ;
@@ -87,19 +88,75 @@ int main (int argc, char *argv[])
     
     if (kh->len != params.w + params.k)
         die ("syncmer len mismatch %d != %d + %d", kh->len, params.w, params.k) ;
+
+    printf("khash length: %d\n", kh->len);
+    printf("khash dimension: %d\n", kh->dim);
+    printf("khash max: %llu\n", kh->max);
+    printf("khash count: %llu\n", kh->count);
     printf ("read %llu kmers from %s\n", kh->max, oneFileName (ofK)) ;
+    
     oneFileClose (ofK) ;
     timeUpdate (stdout) ;
 
-    int i, j ; // general index variables
+    // process the sequences serially
+    I64 totSeq = 0;
+    
+    while (seqIOread(sio)) {
 
-    seqIOclose (sio) ;
-    kmerHashDestroy (kh) ;
-    oneSchemaDestroy (schema) ;				   
-                    
-    timeUpdate (stderr) ;
-    timeTotal (stderr) ;
+        I64 seqLen = sio->seqLen;
+        I64 iSync;
+        I64 seqStart = 0 ;
+        int pos = 0;
+        I64 sync;
+
+        printf("read sequence %s length %" PRIu64 "\n", sqioId(sio), seqLen);
+        char *s = sqioSeq(sio);
+        printf("sequence: %s\n", s);
+        char *id = sqioId(sio);
+        totSeq += seqLen;
+        printf("read sequence %s length %" PRIu64 "\n", sqioId(sio), seqLen);
+        
+        SeqhashIterator *sit = syncmerIterator (sh, s, seqLen) ;
+        while (syncmerNext (sit, 0, &pos, 0)) 
+        {
+            printf("found syncmer at position %" PRIu64 "\n", (uint64_t)pos);
+            sync = 0;
+            if (kmerHashFind (kh, s+pos, &sync)) {
+                printf("found hit: %" PRIu64 "\n", sync);
+                if (sync < 0) sync = -sync ;
+                else sync = 0 ;
+            } else {
+                printf("no hit\n");
+            }
+        }
+        seqhashIteratorDestroy (sit) ;
+    }
+
+    // fill out -- write to .1ref file (using ofOut)
+
+    /*
+    "P 3 ref                   REFERENCE INFORMATION\n"
+    "O N 2 6 STRING 3 INT      name of sequence, e.g. chr1, and its length\n"
+    "O I 1 8 INT_LIST          list of indexes into name table for each sync: 0 if missing, -ve if RC\n"
+    "O P 1 8 INT_LIST          positions in sequence\n"
+
+    for (j = 1 ; j < dictMax (nameDict) ; ++i)
+    { 
+        oneInt(of,1) = arr(chrSize,j,I64) ;
+        char *name = dictName(nameDict,j) ;
+        oneWriteLine (ofOut, 'N', strlen(name), name) ;
+    }
+
+    oneWriteLine (ofOut, 'I', kh->max, index) ;
+    oneWriteLine (ofOut, 'P', kh->max, position) ;
+    */
     
     // clean up memory
+    seqIOclose (sio) ;
+    oneFileClose (ofOut) ;
+    kmerHashDestroy (kh) ;
+    oneSchemaDestroy (schema) ;				   
+    timeUpdate (stderr) ;
+    timeTotal (stderr) ;    
 }
 
